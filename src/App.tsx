b6641359fd2941c79effa17691b009b2960f4148
Ramon-Carrillo <carrillojrramon@gmail.com>
2022-02-04 19:12:33 -0600

    added linear progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,17 @@ const App = () => {
     getProducts,
   )
   console.log(data)
+
+  const getTotalItems = () => null
+
+  const handleAddToCart = () => null
+
+  const handleRemoveFromCart = () => null
+
+  if (isLoading) return <LinearProgress />
+
+  if (error) return <div>Something went wrong...</div>
+
   return <div className='App'>Start</div>
 }
 
